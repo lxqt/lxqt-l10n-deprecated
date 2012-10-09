f45be7760dc1b878771c18f11bd97e0d782fd414
Alec Moskvin <alecm@gmx.com>
2012-10-09 17:23:59 -0400

    Update translations

--- quicklaunch_eu.ts
+++ quicklaunch_eu.ts
@@ -1,28 +1,28 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="eu" version="2.0">
 <context>
     <name>QuickLaunchButton</name>
     <message>
         <source>Move left</source>
-        <translation>Sposta a sinistra</translation>
+        <translation>Mugitu ezkerrera</translation>
     </message>
     <message>
         <source>Move right</source>
-        <translation>Sposta a destra</translation>
+        <translation>Mugitu eskuinera</translation>
     </message>
     <message>
         <source>Remove from quicklaunch</source>
-        <translation>Rimuovi da Avvio rapido</translation>
+        <translation>Kendu abio azkarretik</translation>
     </message>
 </context>
 <context>
     <name>RazorQuickLaunch</name>
     <message>
         <source>Drop Error</source>
-        <translation>Errore di spostamento</translation>
+        <translation>Errorea jaregitean</translation>
     </message>
     <message>
         <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
-        <translation>Il file/URL &apos;%1&apos; in questo momento non può essere inserito in Avvio rapido</translation>
+        <translation>&apos;%1&apos; fitxategia/URLa ezin da QuickLaunch-en txertatu oraingoz</translation>
     </message>
 </context>
 </TS>
--- quicklaunch_pl.ts
+++ quicklaunch_pl.ts
@@ -1,28 +1,28 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.0">
 <context>
     <name>QuickLaunchButton</name>
     <message>
         <source>Move left</source>
-        <translation>Sposta a sinistra</translation>
+        <translation>Przesuń w lewo</translation>
     </message>
     <message>
         <source>Move right</source>
-        <translation>Sposta a destra</translation>
+        <translation>Przesuń w prawo</translation>
     </message>
     <message>
         <source>Remove from quicklaunch</source>
-        <translation>Rimuovi da Avvio rapido</translation>
+        <translation>Usuń z szybkiego uruchamiania</translation>
     </message>
 </context>
 <context>
     <name>RazorQuickLaunch</name>
     <message>
         <source>Drop Error</source>
-        <translation>Errore di spostamento</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
-        <translation>Il file/URL &apos;%1&apos; in questo momento non può essere inserito in Avvio rapido</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
