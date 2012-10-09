74758d62e844f3bb2c2eb6ce4d42fe834875146f
Alec Moskvin <alecm@gmx.com>
2012-10-09 17:23:59 -0400

    Update translations

--- mount_eu.ts
+++ mount_eu.ts
@@ -1,59 +1,59 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="eu" version="2.0">
 <context>
     <name>MenuDiskItem</name>
     <message>
         <source>Click to access this device from other applications.</source>
-        <translation>他のアプリケーションからこのデバイスにアクセスするためにクリックしてください。</translation>
+        <translation>Klikatu gailu hau beste aplikazioetatik atzitzeko.</translation>
     </message>
     <message>
         <source>Click to eject this disc.</source>
-        <translation>ディスクをイジェクトするためにクリックしてください。</translation>
+        <translation>Klikatu diskoa egozteko.</translation>
     </message>
 </context>
 <context>
     <name>MountButton</name>
     <message>
         <source>Removable media/devices manager</source>
-        <translation>リムーバルメディア/デバイス管理</translation>
+        <translation>Eduki/gailu aldagarrien kudeatzailea</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation>デバイス &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; は接続されました。</translation>
+        <translation>&lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;gailua konektatuta.</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation>デバイス &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; は接続解除されました。</translation>
+        <translation>&lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;gailua kenduta.</translation>
     </message>
     <message>
         <source>No devices Available.</source>
-        <translation>利用可能なデバイスはありません。</translation>
+        <translation>Gailurik ez erabilgarri.</translation>
     </message>
 </context>
 <context>
     <name>RazorMountConfiguration</name>
     <message>
         <source>Razor Removable media manager settings</source>
-        <translation>Razor リムーバブルメディア管理の設定</translation>
+        <translation>Razor gailu aldagarrien kudeatzailearen ezarpenak</translation>
     </message>
     <message>
         <source>Behaviour</source>
-        <translation>挙動</translation>
+        <translation>Portaera</translation>
     </message>
     <message>
         <source>When a device is connected </source>
-        <translation>デバイスが接続されている場合</translation>
+        <translation>Gailu bat konektatzean</translation>
     </message>
     <message>
         <source>Popup menu</source>
-        <translation>ポップアップメニュー</translation>
+        <translation>Laster-menua</translation>
     </message>
     <message>
         <source>Show info</source>
-        <translation>情報を表示</translation>
+        <translation>Erakutsi informazioa</translation>
     </message>
     <message>
         <source>Do nothing</source>
-        <translation>何もしない</translation>
+        <translation>Ez egin ezer</translation>
     </message>
 </context>
 </TS>
