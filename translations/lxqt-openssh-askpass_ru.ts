7b8cb7576c40588fe9c8e9aeca312b49c9a326f3
Ilya87 <yast4ik@yahoo.com>
2014-10-19 10:11:00 +0400

    Add Russian translation

---
+++ translations/lxqt-openssh-askpass_ru.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Запрос пароля для аутентификации по OpenSSH</translation>
+    </message>
+    <message>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Введите ваш SSH-пароль для запроса:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>unknown request</source>
+        <translation>неизвестный запрос</translation>
+    </message>
+</context>
+</TS>
