b8fac991cae9c709b892973aa681ecd3d55a3e01
Sérgio Marques <smarquespt@gmail.com>
2015-01-07 10:26:56 +0000

    Portuguese update

---
+++ translations/lxqt-openssh-askpass_pt.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Pedido de senha de autenticação OpenSSH</translation>
+    </message>
+    <message>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Escreva a sua senha SSH para o pedido:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>unknown request</source>
+        <translation>pedido desconhecido</translation>
+    </message>
+</context>
+</TS>
