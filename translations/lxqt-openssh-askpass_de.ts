f7b4abe9fdc03a9f8c8c3d2fa9365eaaac8a67aa
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-01-25 12:18:56 +0100

    Added german translation.

---
+++ translations/lxqt-openssh-askpass_de.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Abfrage der OpenSSH Authentisierungs-Passphrase</translation>
+    </message>
+    <message>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>SSH-Passphrase eingeben:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>unknown request</source>
+        <translation>Unbekannte Abfrage</translation>
+    </message>
+</context>
+</TS>
