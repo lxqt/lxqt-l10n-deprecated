52d98dbb872286b052c8ab56ab2454a2446cf6e0
Dimitrios Glentadakis <dglent@free.fr>
2015-08-30 07:18:27 +0200

    Add Greek (el) translation

---
+++ translations/lxqt-openssh-askpass_el.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Αίτηση συνθηματικού ταυτοποίησης OpenSSH</translation>
+    </message>
+    <message>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Εισαγάγετε το συνθηματικό σας SSH για αίτηση:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>unknown request</source>
+        <translation>άγνωστη αίτηση</translation>
+    </message>
+</context>
+</TS>
