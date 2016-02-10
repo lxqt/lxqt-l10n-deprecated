e607ffb2093a820ffee2acf38b11684b5eab147e
Bruno Fosados <bruno.fosados@gmail.com>
2016-02-10 02:00:54 -0600

    Create lxqt-sudo_es.ts
    Complete translation of lxqt-sudo into Spanish

---
+++ lxqt-sudo/lxqt-sudo_es.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>Communication</name>
+    <message>
+        <location filename="../communication.cpp" line="85"/>
+        <source>%1: Communication - unable to create shared memory(%2B), </source>
+        <translation>%1: Comunicación - incapaz de crear memoria compartida(% 2B), </translation>
+    </message>
+    <message>
+        <location filename="../communication.cpp" line="183"/>
+        <source>%1: Communication - password is too long(%2) for aquired shared memory(%3), has to be cut-off...
+</source>
+        <translation>%1: Comunicación - contraseña es demasiada larga(% 2) para la memoria compartida adquirida(% 3), tiene que ser recortada...
+</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="20"/>
+        <location filename="../build/ui_passworddialog.h" line="129"/>
+        <source>LXQt sudo</source>
+        <translation>sudo LXQt</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="77"/>
+        <location filename="../build/ui_passworddialog.h" line="134"/>
+        <source>Command:</source>
+        <translation>Comando:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="84"/>
+        <location filename="../build/ui_passworddialog.h" line="135"/>
+        <source>Password:</source>
+        <translation>Contraseña:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; necesita privilegios administrativos.
+Por favor, introduzca su contraseña.</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="53"/>
+        <source>Attempt #%1</source>
+        <translation>Intento #%1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="58"/>
+        <source>Usage: %1 command [arguments...]
+
+GUI frontend for %2
+
+Arguments:
+  command        Command to run.
+  arguments      Optional arguments for command.
+
+</source>
+        <translation>Uso: %1 comando [argumentos...]
+
+Interfaz gráfica para %2
+
+Argumentos:
+  comando        Comando a ejecutar.
+  argumentos     Argumentos opcionales para el comando.
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="70"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 versión %2
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="81"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: Ningún comando a ejecutar fue suministrado!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="156"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>Sub-proceso &apos;%1&apos; fallido!
+%2</translation>
+    </message>
+</context>
+</TS>
