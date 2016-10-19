d384c8b16ecf426849687a32f619981d1290ca39
paiiou <paiiou@free.fr>
2016-10-19 10:18:38 +0200

    Update French translations
    liblxqt, about, notificationd, openssh-askpass, policykit,
    powermanagement, runner, session, sudo

---
+++ lxqt-openssh-askpass/lxqt-openssh-askpass_fr.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Demande de la PassPhrase de OpenSSH</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Entrez votre PassPhrase de SSH :</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="39"/>
+        <source>unknown request</source>
+        <translation>Demande inconnue</translation>
+    </message>
+</context>
+</TS>
