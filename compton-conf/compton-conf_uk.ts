471d8e6f1341a44f5607c8e1b7ca8765811c0e4b
knedlyk <yupadmin@gmail.com>
2016-11-03 01:42:46 +0100

    compton-conf Ukrainian translation

--- compton-conf/compton-conf_uk.ts
+++ compton-conf/compton-conf_uk.ts
@@ -71,88 +71,88 @@
     <message>
         <location filename="../../maindialog.ui" line="138"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Непрозорість</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="147"/>
         <source>The opacity for menus</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Непрозорість меню</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="154"/>
         <source>Default opacity of inactive windows</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Типова непрозорість неактивних вікон</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="161"/>
         <source>Default opacity for active windows</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Типова непрозорість активних вікон</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="168"/>
         <source>Opacity of window titlebars and borders</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Непрозорість панелі заголовку вікон і меж</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="175"/>
         <source>XRender backend: Step size for alpha pictures
 (Increasing it may result in less X resource usage)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">XRender backend: Розмір кроку для альфа-малюнків (Збільшення цього може приводити до меншого споживання ресурсів X)</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="183"/>
         <source>Dim inactive windows</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Матовість неактивних вікон</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="190"/>
         <source>Do not let dimness adjust based on window opacity</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Не давати підлаштовувати матовість на основі непрозорості вікна</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="197"/>
         <source>Blur background of transparent windows</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Розмиття тла прозорих вікон</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="204"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Розмиття тла непрозорих вікон разом з прозорими рамками</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="211"/>
         <source>Do not let blur radius adjust based on window opacity</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Не налаштовувати радіус розмиття на основі непрозорості вікна</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="279"/>
         <source>Fade</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Згортання</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="285"/>
         <source>Fade windows during opacity changes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Згортання вікна під час зміни непрозорості </translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="292"/>
         <source>The time between steps in a fade in milliseconds</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Час між кроками згортання в мілісекундах</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="299"/>
         <source>Opacity change between steps while fading in</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Зміна непрозорості між кроками під час згортання</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="306"/>
         <source>Opacity change between steps while fading out</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Зміна непрозорості між кроками під час розгортання</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="313"/>
         <source>Avoid fade windows in/out when opening/closing</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Уникати згортання/розгортання вікон при відкриванні/закриванні</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="356"/>
