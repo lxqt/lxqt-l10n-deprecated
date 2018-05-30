f1d09c7bb4b65c242182bcbcd3845ef72a8bfc16
Alf Gaida <agaida@siduction.org>
2018-05-30 22:17:33 +0200

    Revert "lxqt-panel: Remove plugin-clock (#351)"
    This reverts commit 5723c7b9f60b98b0d8c12ab379e5e63232162e7f.

---
+++ lxqt-panel/plugin-clock/clock_da.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>FirstDayCombo</name>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="49"/>
+        <source>&lt;locale based&gt;</source>
+        <translation>&lt;baseret på sprogvalg&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtClock</name>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="57"/>
+        <source>Date&amp;Time (clock) plugin is deprecated</source>
+        <translation>Dato og klokkeslæt-pluginet (ur) er udgået</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="58"/>
+        <source>The &lt;strong&gt;clock&lt;/strong&gt; plugin is deprecated and will be removed in future version of LXQt. Consider replacing it with &lt;strong&gt;worldclock&lt;/strong&gt;.&lt;br/&gt;</source>
+        <translation>&lt;strong&gt;Ur&lt;/strong&gt;-pluginet er udgået og vil blive fjernet i fremtidige versioner af LXQt. Overvej at erstatte det med &lt;strong&gt;verdensur&lt;/strong&gt;.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="60"/>
+        <source>don&apos;t show this again</source>
+        <translation>vis det ikke igen</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtClockConfiguration</name>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="14"/>
+        <source>Clock Settings</source>
+        <translation>Ur-indstillinger</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="20"/>
+        <source>Time</source>
+        <translation>Klokkeslæt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="26"/>
+        <source>&amp;Show seconds</source>
+        <translation>&amp;Vis sekunder</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="33"/>
+        <source>12 &amp;hour style</source>
+        <translation>12-&amp;timers-stil</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="40"/>
+        <source>&amp;Use UTC</source>
+        <translation>&amp;Brug UTC</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="59"/>
+        <source>Date &amp;format</source>
+        <translation>Dato&amp;format</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="76"/>
+        <source>&amp;Do not show date</source>
+        <translation>&amp;Vis ikke dato</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="86"/>
+        <source>Show date &amp;before time</source>
+        <translation>Vis dato &amp;før klokkeslæt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="93"/>
+        <source>Show date &amp;after time</source>
+        <translation>Vis dato &amp;efter klokkeslæt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="100"/>
+        <source>Show date below time on new &amp;line</source>
+        <translation>Vis dato under klokkeslæt på ny &amp;linje</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="107"/>
+        <source>First day of week in calendar</source>
+        <translation>Første dag på ugen i kalender</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="124"/>
+        <source>Orientation</source>
+        <translation>Orientering</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="130"/>
+        <source>Auto&amp;rotate when the panel is vertical</source>
+        <translation>Automatisk &amp;rotering når panelet er lodret</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="50"/>
+        <source>Date</source>
+        <translation>Dato</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="268"/>
+        <source>Input custom date format</source>
+        <translation>Input brugerdefineret datoformat</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="268"/>
+        <source>Interpreted sequences of date format are:
+
+d	the day as number without a leading zero (1 to 31)
+dd	the day as number with a leading zero (01 to 31)
+ddd	the abbreviated localized day name (e.g. &apos;Mon&apos; to &apos;Sun&apos;).
+dddd	the long localized day name (e.g. &apos;Monday&apos; to &apos;Sunday&apos;).
+M	the month as number without a leading zero (1-12)
+MM	the month as number with a leading zero (01-12)
+MMM	the abbreviated localized month name (e.g. &apos;Jan&apos; to &apos;Dec&apos;).
+MMMM	the long localized month name (e.g. &apos;January&apos; to &apos;December&apos;).
+yy	the year as two digit number (00-99)
+yyyy	the year as four digit number
+
+All other input characters will be treated as text.
+Any sequence of characters that are enclosed in single quotes (&apos;)
+will also be treated as text and not be used as an expression.
+
+
+Custom date format:</source>
+        <translation>Fortolket sekvenser af datoformat er:
+
+d	dagen som tal uden et foranstillet nul (1 til 31)
+dd	dagen som tal med et foranstillet nul (01 til 31)
+ddd	det forkortet oversatte navn for dagen (f.eks. &apos;Man&apos; til &apos;Søn&apos;).
+dddd	det lange oversatte navn for dagen (f.eks. &apos;Mandag&apos; til &apos;Søndag&apos;).
+M	måneden som tal uden et foranstillet nul (1-12)
+MM	måneden som tal med et foranstillet nul (01-12)
+MMM	det forkortet oversatte navn for måneden (f.eks. Jan&apos; til Dec&apos;).
+MMMM	det lange oversatte navn for måneden (f.eks. Januar&apos; til December&apos;).
+yy	året som tocifret tal (00-99)
+yyyy	året som firecifret tal
+
+Alle andre input-tegn vil blive behandler som tekst.
+Sekvenser af tegn som er omgivet af enkelte anførselstegn (&apos;)
+vil blive behandlet som tekst og ikke blive brugt som et udtryk.
+
+
+Brugerdefineret datoformat:</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-panel/plugin-clock/clock_lt.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>FirstDayCombo</name>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="49"/>
+        <source>&lt;locale based&gt;</source>
+        <translation>&lt;pagal lokalę&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtClock</name>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="57"/>
+        <source>Date&amp;Time (clock) plugin is deprecated</source>
+        <translation>Datos ir laiko (laikrodžio) įskiepis yra apleistas</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="58"/>
+        <source>The &lt;strong&gt;clock&lt;/strong&gt; plugin is deprecated and will be removed in future version of LXQt. Consider replacing it with &lt;strong&gt;worldclock&lt;/strong&gt;.&lt;br/&gt;</source>
+        <translation>&lt;strong&gt;Laikrodžio&lt;/strong&gt; įskiepis yra apleistas ir būsimosiose LXQt versijose bus pašalintas. Apsvarstykite galimybę jį pakeisti &lt;strong&gt;pasaulio laikrodžių&lt;/strong&gt; įskiepiu.&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclock.cpp" line="60"/>
+        <source>don&apos;t show this again</source>
+        <translation>daugiau neberodyti</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtClockConfiguration</name>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="14"/>
+        <source>Clock Settings</source>
+        <translation>Laikrodžio nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="20"/>
+        <source>Time</source>
+        <translation>Laikas</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="26"/>
+        <source>&amp;Show seconds</source>
+        <translation>&amp;Rodyti sekundes</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="33"/>
+        <source>12 &amp;hour style</source>
+        <translation>12 &amp;valandų stilius</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="40"/>
+        <source>&amp;Use UTC</source>
+        <translation>&amp;Naudoti suderintąjį pasaulinį laiką (UTC)</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="76"/>
+        <source>&amp;Do not show date</source>
+        <translation>&amp;Nerodyti datą</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="86"/>
+        <source>Show date &amp;before time</source>
+        <translation>Rodyti datą pr&amp;ieš laiką</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="93"/>
+        <source>Show date &amp;after time</source>
+        <translation>Rodyti datą p&amp;o laiko</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="100"/>
+        <source>Show date below time on new &amp;line</source>
+        <translation>Rodyti datą žemiau laiko, naujoje ei&amp;lutėje</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="107"/>
+        <source>First day of week in calendar</source>
+        <translation>Pirma savaitės diena kalendoriuje</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="124"/>
+        <source>Orientation</source>
+        <translation>Orientacija</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="130"/>
+        <source>Auto&amp;rotate when the panel is vertical</source>
+        <translation>Automatiškai pa&amp;sukti, kai skydelis yra vertikalus</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="50"/>
+        <source>Date</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.ui" line="59"/>
+        <source>Date &amp;format</source>
+        <translation>Datos &amp;formatas</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="268"/>
+        <source>Input custom date format</source>
+        <translation>Įveskite tinkintą datos formatą</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtclockconfiguration.cpp" line="268"/>
+        <source>Interpreted sequences of date format are:
+
+d	the day as number without a leading zero (1 to 31)
+dd	the day as number with a leading zero (01 to 31)
+ddd	the abbreviated localized day name (e.g. &apos;Mon&apos; to &apos;Sun&apos;).
+dddd	the long localized day name (e.g. &apos;Monday&apos; to &apos;Sunday&apos;).
+M	the month as number without a leading zero (1-12)
+MM	the month as number with a leading zero (01-12)
+MMM	the abbreviated localized month name (e.g. &apos;Jan&apos; to &apos;Dec&apos;).
+MMMM	the long localized month name (e.g. &apos;January&apos; to &apos;December&apos;).
+yy	the year as two digit number (00-99)
+yyyy	the year as four digit number
+
+All other input characters will be treated as text.
+Any sequence of characters that are enclosed in single quotes (&apos;)
+will also be treated as text and not be used as an expression.
+
+
+Custom date format:</source>
+        <translation>Suprantamos datos formato sekos yra:
+
+d	diena kaip skaičius be priekinio nulio (1 iki 31)
+dd	diena kaip skaičius su priekiniu nuliu (01 iki 31)
+ddd	sutrumpintas lokalizuotas dienos pavadinimas (pvz., &quot;Pir&quot; iki &quot;Sek&quot;).
+dddd	ilgas lokalizuotas dienos pavadinimas (pvz., &quot;Pirmadienis&quot; iki &quot;Sekmadienis&quot;).
+M	mėnesis kaip skaičius be priekinio nulio (1-12)
+MM	mėnesis kaip skaičius su priekiniu nuliu (01-12)
+MMM	sutrumpintas lokalizuotas mėnesio pavadinimas (pvz., &quot;Sau&quot; iki &quot;Gru&quot;).
+MMMM	ilgas lokalizuotas mėnesio pavadinimas (pvz., &quot;Sausis&quot; iki &quot;Gruodis&quot;).
+yy	metai kaip dviejų skaitmenų skaičius (00-99)
+yyyy	metai kaip keturių skaitmenų skaičius
+
+Visi kiti įvesti simboliai bus laikomi tekstu.
+Bet kokia simbolių seka, patalpinta į kabutes (&apos;),
+taip pat bus laikoma tekstu ir nebus naudojama kaip reiškinys.
+
+
+Tinkintas datos formatas:</translation>
+    </message>
+</context>
+</TS>
